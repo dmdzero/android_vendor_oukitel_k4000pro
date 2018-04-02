72adb201ac14c7e51bcedbe726256125274b53f0
Danijel Maloca Damjanovic <dmdzero@gmail.com>
2018-04-02 15:04:55 -0700

    my first

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-Vendor Tree for CM 12.1 Oukitel k4000(orange)
+test Vendor Tree for CM 15.1 Oukitel k4000pro forked from vitek999
 ==================================
 Thanks to:
 @darklord4822
 @olegsvs
 @Deepflex
 @visi0nary
+@vitek999
---
+++ k4000pro-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k4000pro/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k4000pro/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k4000pro/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k4000pro/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k4000pro/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k4000pro/vendor,system/vendor)
